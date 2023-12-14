f8410eaa78780b1d12cc8b6b5fb99e0ed0a708f4
kevchristian <135082813+kevchristian@users.noreply.github.com>
2023-12-14 21:33:51 +0700

    upload

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,3 +1,4 @@
+import multer from "multer";
 
 export const home = (req,res) => {
     res.render("index");
@@ -13,3 +14,27 @@ export const scatter = (req,res) => {
 export const addData = (req,res) => {
     res.render("tambah_data");
 }
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/upload/'); 
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+export const uploadData = (req, res, next) => {
+    upload.single('data')(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            return res.render("tambah_data", { error: 'An error occurred while uploading files' });
+        } else if (err) {
+            return res.status(500).json({ error: 'An error occurred while uploading files' });
+        }
+        // Successful upload logic
+        res.render("tambah_data", { success: 'File uploaded successfully!' });
+    });
+};
+
