936616372bbcbe8546149b144f1fe90824217e44
Vico Pratama <70521809+vicsprat@users.noreply.github.com>
2023-12-29 14:41:47 +0700

    inisiasi dropdown

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var selectDropdownButton = document.getElementById('selectDropdown');
-    var dropdownMenu = document.getElementById('dropdownMenu');
+const fitur1 = document.querySelector("#fitur1");
+const fitur2 = document.querySelector("#fitur2");
 
-    dropdownMenu.addEventListener('click', function (event) {
+const listFitur = ['ID', 'Year_Birth',	'Education',	'Marital_Status',	'Income',	'Kidhome',	'Teenhome',	'Dt_Customer',	'Recency', 'MntWines',	'MntFruits',	'MntMeatProducts',	'MntFishProducts',	'MntSweetProducts',	'MntGoldProds',	'NumDealsPurchases',	'NumWebPurchases',	'NumCatalogPurchases',	'NumStorePurchases',	'NumWebVisitsMonth', 'AcceptedCmp3'	,'AcceptedCmp4',	'AcceptedCmp5'	, 'AcceptedCmp1', 'AcceptedCmp2',	'Complain'	,'Z_CostContact',	'Z_Revenue'	, 'Response'];
+const indexNumerikal = [4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+
+for(let fitur of listFitur){
+    let content1 = `<li><a class="dropdown-item">${fitur}</a></li>`;
+    let content2 = `<li> 
+    <label> 
+      <input type="checkbox" 
+             value="${fitur}"> 
+          ${fitur}
+      </label> 
+  </li> `;
+    fitur1.innerHTML += content1;
+    fitur2.innerHTML += content2;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var selectDropdown = document.getElementById('selectDropdown');
+
+    fitur1.addEventListener('click', function (event) {
       if (event.target.classList.contains('dropdown-item')) {
-        selectDropdownButton.textContent = event.target.textContent;
+        selectDropdown.textContent = event.target.textContent;
       }
     });
 });
@@ -29,4 +48,3 @@ function handleCB() {
 chBoxes.forEach((checkbox) => { 
     checkbox.addEventListener('change', handleCB); 
 }); 
-
