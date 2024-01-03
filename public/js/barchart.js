284b72333e912b87bb70c4df598c0d87988a2d69
miroslavmatthew <90718789+miroslavmatthew@users.noreply.github.com>
2024-01-03 21:59:11 +0700

    revisi barchart

--- public/js/barchart.js
+++ public/js/barchart.js
@@ -4,14 +4,14 @@ const aggregasi = document.querySelector("#aggregasi");
 const canvas = document.querySelector("#canvas");
 const listFitur = ['ID', 'Year_Birth',	'Education',	'Marital_Status',	'Income',	'Kidhome',	'Teenhome',	'Dt_Customer',	'Recency', 'MntWines',	'MntFruits',	'MntMeatProducts',	'MntFishProducts',	'MntSweetProducts',	'MntGoldProds',	'NumDealsPurchases',	'NumWebPurchases',	'NumCatalogPurchases',	'NumStorePurchases',	'NumWebVisitsMonth', 'AcceptedCmp3'	,'AcceptedCmp4',	'AcceptedCmp5'	, 'AcceptedCmp1', 'AcceptedCmp2',	'Complain'	,'Z_CostContact',	'Z_Revenue'	, 'Response'];
 const indexNumerikal = [4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-
+const aggregateFunction = ['SUM','COUNT','AVG','MIN','MAX'];
 
 for(let i = 0; i < listFitur.length; i++){
     let content1 = `<li><a class="dropdown-item">${listFitur[i]}</a></li>`;
     let content2 = 
     `<li><a class="dropdown-item2">${listFitur[i]}</a></li>`;
-    fitur1.innerHTML += content1;
-    if(indexNumerikal.includes(i)) fitur2.innerHTML += content2;
+    fitur2.innerHTML += content2;
+    if(!indexNumerikal.includes(i)) fitur1.innerHTML += content1;
 }
 
 const dpBtn = document.getElementById('multiSelectDropdown'); 
@@ -41,11 +41,20 @@ document.addEventListener('DOMContentLoaded', function () {
             }
             
         }
+        if(colome!=undefined){
+          const responseRes = await fetch(`reqGroupByRes?group=${groupedBy}&agg=${aggMethod}&col=${colome}`);
+            const textRes = await responseRes.text();
+            const result = JSON.parse(textRes).result;
+                dataCategory=[];
+                laber = aggMethod + " of " + colome;
+            for(let i=0; i < result.length; i++){
+                dataCategory.push(result[i].result);
+            }
+        }
         generateBarchart();
         aggBtn.disabled = false;
     }
 });
-console.log(colome==undefined);
 aggregasi.addEventListener('click', async function (event) {
     if (event.target.classList.contains('dropdown-item')) {
         let choose = event.target.textContent
@@ -74,6 +83,18 @@ fitur2.addEventListener('click', async function (event) {
       let choose = event.target.textContent
       colome = choose;
       dpBtn.textContent = choose;
+      if(!indexNumerikal.includes(listFitur.indexOf(colome))&&!indexNumerikal.includes(listFitur.indexOf(groupedBy))){
+        aggregasi.innerHTML="";
+        aggregasi.innerHTML+='<li><a class="dropdown-item">COUNT</a></li>';
+        aggMethod = "COUNT";
+        aggBtn.textContent = "COUNT";
+      }
+      else{
+        aggregasi.innerHTML="";
+        for (let i = 0; i < aggregateFunction.length; i++) {
+          aggregasi.innerHTML+=`<li><a class="dropdown-item">${aggregateFunction[i]}</a></li>`
+        }
+      }
       const responseRes = await fetch(`reqGroupByRes?group=${groupedBy}&agg=${aggMethod}&col=${colome}`);
       const textRes = await responseRes.text();
       const result = JSON.parse(textRes).result;
